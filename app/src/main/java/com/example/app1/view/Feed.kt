7b2139c6b18d310157e129ca3b31c677bc20921e
victorluiz3315 <140361500+victorluiz3315@users.noreply.github.com>
2023-10-11 16:12:22 -0300

    profile

--- app/src/main/java/com/example/app1/view/Feed.kt
+++ app/src/main/java/com/example/app1/view/Feed.kt
@@ -1,12 +1,24 @@
 package com.example.app1.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.app1.R
+import com.example.app1.databinding.ActivityMainBinding
 
 class recover : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recover)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rec.setOnClickListener {
+
+            val requestLogin = Intent(this,login::class.java)
+            startActivity(requestLogin)
+        }
     }
 }
--- app/src/main/java/com/example/app1/view/Rec.kt
+++ app/src/main/java/com/example/app1/view/Rec.kt
@@ -1,12 +1,16 @@
 package com.example.app1.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.app1.R
+import com.example.app1.databinding.ActivityMainBinding
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
