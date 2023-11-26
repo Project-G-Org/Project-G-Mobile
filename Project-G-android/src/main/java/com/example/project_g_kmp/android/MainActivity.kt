d3cbd6a4a671d88718650b9cfe9be4c2558b99a3
Carlos Guilherme Miranda Finelli <carlosmfinelli@gmail.com>
2023-11-26 13:09:42 -0300

    Implementação: Navigation
    Implementação e configuração da navegação entre as telas no escopo geral do projeto.

--- Project-G-android/src/main/java/com/example/project_g_kmp/android/MainActivity.kt
+++ Project-G-android/src/main/java/com/example/project_g_kmp/android/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.project_g_kmp.android.navigation.MainNavigation
 import com.example.project_g_kmp.android.view.Feed
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Feed()
+            val navController = rememberNavController()
+            MainNavigation(navController = navController)
         }
     }
 }
