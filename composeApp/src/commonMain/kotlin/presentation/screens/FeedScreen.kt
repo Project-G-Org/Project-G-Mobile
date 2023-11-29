5516bfc5673d63d2b2cc612a357550bbf297f826
Carlos Guilherme Miranda Finelli <carlosmfinelli@gmail.com>
2023-11-29 17:28:25 -0300

    Implementação: Tela recover e Feed
    Implementação da tela de Recover, replicando a tela real com todos os componentes e elementos que devem estar presentes.
    
    Implementação da tela de Feed, replicando a tela real com a maioria dos elementos e componentes que devem estar presentes.
    
    TODO: Corrigir a implementação dos Cards na tela de Feed.

---
+++ composeApp/src/commonMain/kotlin/presentation/screens/FeedScreen.kt
@@ -0,0 +1,514 @@
+package presentation.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+fun Feed(modifier: Modifier = Modifier.background(Color(0xFFEBEBEB))){
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFEBEBEB))
+    ) {
+
+        Column(
+            Modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(10.dp, alignment = Alignment.Top),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Row(
+                modifier = Modifier
+                .padding(0.dp, 5.dp)
+                .horizontalScroll(rememberScrollState())
+            ) {
+                Image(
+                    painter = painterResource("carr1.png"),
+                    contentDescription = null,
+                    Modifier
+                        .size(80.dp)
+                )
+                Image(
+                    painter = painterResource("carr2.png"),
+                    contentDescription = null,
+                    Modifier
+                        .size(80.dp)
+                )
+                Image(
+                    painter = painterResource("carr3.png"),
+                    contentDescription = null,
+                    Modifier
+                        .size(80.dp)
+                )
+                Image(
+                   painter = painterResource("carr4.png"),
+                    contentDescription = null,
+                    Modifier
+                        .size(80.dp)
+                )
+                Image(
+                    painter = painterResource("carr5.png"),
+                    contentDescription = null,
+                    Modifier
+                        .size(80.dp)
+                )
+                Image(
+                    painter = painterResource("carr6.png"),
+                    contentDescription = null,
+                    Modifier
+                        .size(80.dp)
+                )
+                Image(
+                    painter = painterResource("carr7.png"),
+                    contentDescription = null,
+                    Modifier
+                        .size(80.dp)
+                )
+                Image(
+                    painter = painterResource("carr8.png"),
+                    contentDescription = null,
+                    Modifier
+                        .size(80.dp)
+                )
+            }
+
+            Card(
+                elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+            ) {
+
+            }
+
+            Card(
+                elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White,
+                ),
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(all = 10.dp)
+                        .horizontalScroll(rememberScrollState())
+                        .fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource("carr1.png"),
+                        contentDescription = null,
+                        Modifier
+                            .size(60.dp)
+                    )
+
+                    Column(
+                        modifier = Modifier
+                            .padding(3.dp, 10.dp)
+                    ){
+
+                        Text(
+                            "marcelo",
+                            color = Color(0xFFE5684A),
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                        Text(
+                            text = "MARANHAO",
+                            color = Color.Black,
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                    }
+
+                }
+                SliderBanner()
+//                LazyRow(
+//                    modifier = Modifier
+//                        .padding(all = 10.dp)
+//                        .clip(RoundedCornerShape(18.dp))
+////                        .horizontalScroll(rememberScrollState())
+//                        .fillMaxWidth()
+//                ) {
+//                    item {
+//                        Image(
+//                            painter = painterResource(id = R.drawable.backlogin),
+//                            contentDescription = null,
+//                            contentScale = ContentScale.FillBounds,
+//                            modifier = Modifier
+//                                .width(300.dp)
+//                                .height(500.dp)
+//                                .padding(horizontal = 15.dp)
+//                        )
+//                    }
+//                    item {
+//                        Image(
+//                            painter = painterResource(id = R.drawable.backlogin),
+//                            contentDescription = null,
+//                            contentScale = ContentScale.FillBounds,
+//                            modifier = Modifier
+//                                .width(300.dp)
+//                                .height(500.dp)
+//                                .padding(horizontal = 15.dp)
+//                        )
+//                    }
+//                }
+//                Box(
+//                    modifier = Modifier
+//                        .padding(all = 10.dp)
+//                        .clip(RoundedCornerShape(18.dp))
+////                        .horizontalScroll(rememberScrollState())
+//                        .fillMaxWidth()
+//                        .scrollable(rememberScrollableState {
+//                            it
+//                        }, Orientation.Horizontal)
+//                ) {
+//                    Image(
+//                        painter = painterResource(id = R.drawable.backlogin),
+//                        contentDescription = null,
+//                        contentScale = ContentScale.FillBounds,
+//                        modifier = Modifier
+//                            .width(300.dp)
+//                            .height(500.dp)
+//                            .background(Color.Red)
+//                    )
+//                    Image(
+//                        painter = painterResource(id = R.drawable.backlogin),
+//                        contentDescription = null,
+//                        contentScale = ContentScale.FillBounds,
+//                        modifier = Modifier
+//                            .width(300.dp)
+//                            .height(500.dp)
+//                            .background(Color.Red)
+//                    )
+//                }
+
+                Row(
+                    modifier = Modifier
+                    .height(40.dp)
+                    .fillMaxWidth()
+                    .background(color = Color.White),
+                    verticalAlignment = Alignment.Top,
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+
+                    Icon(
+                        painterResource("baseline_mode_comment_24"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+
+                    Icon(
+                        painterResource("baseline_thumb_up_24"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+
+                    Icon(
+                        painterResource("baseline_share_24"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+
+                    Icon(
+                        painterResource("baseline_save_alt_24"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+
+                }
+            }
+
+            Card(
+                elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White,
+                ),
+                modifier = Modifier
+                    .padding(10.dp, 5.dp)
+                    .fillMaxWidth(),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(all = 10.dp)
+                        .horizontalScroll(rememberScrollState())
+                        .fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource("perf2.png"),
+                        contentDescription = null,
+                        Modifier
+                            .size(60.dp)
+                    )
+
+                    Column(
+                        modifier = Modifier
+                            .padding(3.dp, 10.dp)
+                    ){
+
+                        Text(
+                            "dojavam",
+                            color = Color(0xFFE5684A),
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                        Text(
+                            text = "EUA CALIFORMIA",
+                            color = Color.Black,
+                            fontSize = 12.sp,
+                        )
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .padding(25.dp, 5.dp)
+                        .fillMaxWidth()
+                ) {
+
+                    Text(
+                        text = "Et eius voluptate dolorum debitis ducimus fugit molestiae dolor. Eum et dolores aut reprehenderit dignissimos nihil error. Ducimus voluptas velit laboriosam provident. Labore est quis ad eum sed. Ratione quibusdam fugiat. Qui labore quisquam dolorum maiores deserunt dolores nihil.Et eius voluptate dolorum debitis ducimus fugit molestiae dolor. Eum et dolores aut reprehenderit dignissimos nihil error. Ducimus voluptas velit laboriosam provident. Labore est quis ad eum sed. Ratione quibusdam fugiat. Qui labore quisquam dolorum maiores deserunt dolores nihil.",
+                        color = Color.Black,
+                        fontSize = 12.sp,
+                    )
+
+                }
+
+                Column(
+                    modifier = Modifier
+                        .padding(15.dp)
+                        .clip(RoundedCornerShape(18.dp))
+                        .fillMaxWidth()
+                ) {
+
+                    Image(
+                        painter = painterResource("post4.png"),
+                        contentDescription = null,
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(600.dp)
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                    .height(40.dp)
+                    .fillMaxWidth()
+                    .background(color = Color.White),
+                    verticalAlignment = Alignment.Top,
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+
+                    Icon(
+                        painterResource("baseline_mode_comment_24.xml"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+
+                    Icon(
+                        painterResource("baseline_thumb_up_24.xml"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+
+                    Icon(
+                        painterResource("baseline_share_24.xml"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+
+                    Icon(
+                        painterResource("baseline_save_alt_24.xml"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+                }
+            }
+
+            Card(
+                elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White,
+                ),
+                modifier = Modifier
+                    .padding(10.dp, 5.dp)
+                    .fillMaxWidth(),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(all = 10.dp)
+                        .horizontalScroll(rememberScrollState())
+                        .fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource("perf2.png"),
+                        contentDescription = null,
+                        Modifier
+                            .size(60.dp)
+                    )
+
+                    Column(
+                        modifier = Modifier
+                            .padding(3.dp, 10.dp)
+                    ){
+
+                        Text(
+                            "dojavam",
+                            color = Color(0xFFE5684A),
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                        Text(
+                            text = "EUA CALIFORMIA",
+                            color = Color.Black,
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                    }
+
+                }
+
+                Column(
+                    modifier = Modifier
+                        .padding(25.dp, 5.dp)
+                        .fillMaxWidth()
+                ) {
+
+                    Text(
+                        text = "Et eius voluptate dolorum debitis ducimus fugit molestiae dolor. Eum et dolores aut reprehenderit dignissimos nihil error. Ducimus voluptas velit laboriosam provident. Labore est quis ad eum sed. Ratione quibusdam fugiat. Qui labore quisquam dolorum maiores deserunt dolores nihil.Et eius voluptate dolorum debitis ducimus fugit molestiae dolor. Eum et dolores aut reprehenderit dignissimos nihil error. Ducimus voluptas velit laboriosam provident. Labore est quis ad eum sed. Ratione quibusdam fugiat. Qui labore quisquam dolorum maiores deserunt dolores nihil.",
+                        color = Color.Black,
+                        fontSize = 12.sp,
+                    )
+
+                }
+
+                Row(
+                    modifier = Modifier
+                    .height(40.dp)
+                    .fillMaxWidth()
+                    .background(color = Color.White),
+                    verticalAlignment = Alignment.Top,
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+
+                    Icon(
+                        painterResource("baseline_mode_comment_24.xml"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+
+                    Icon(
+                        painterResource("baseline_thumb_up_24.xml"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+
+                    Icon(
+                        painterResource("baseline_share_24.xml"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+
+                    Icon(
+                        painterResource("baseline_save_alt_24.xml"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+                }
+            }
+        }
+    }
+
+    Column(
+        Modifier
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom
+    ){
+
+
+
+        Row(
+            modifier = Modifier
+            .height(60.dp)
+            .fillMaxWidth()
+            .background(color = Color(0xFF202020)),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+
+            Icon(
+                painterResource("baseline_add_24.xml"),
+                contentDescription = null,
+                Modifier.size(30.dp),
+                tint = Color(0xFFE5684A)
+            )
+
+            Icon(
+                painterResource("baseline_home_24.xml"),
+                contentDescription = null,
+                Modifier.size(30.dp),
+                tint = Color(0xFFE5684A)
+            )
+
+            Icon(
+                painterResource("baseline_search_24.xml"),
+                contentDescription = null,
+                Modifier.size(30.dp),
+                tint = Color(0xFFE5684A)
+            )
+
+            Icon(
+                painterResource("baseline_person_24.xml"),
+                contentDescription = null,
+                Modifier.size(30.dp),
+                tint = Color(0xFFE5684A)
+            )
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/presentation/screens/RecoverScreen.kt
@@ -0,0 +1,91 @@
+package presentation.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import model.InputType
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import presentation.Components.CustomTextInput
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+fun RecoverScreen() {
+
+    val valueGmail = remember {
+        mutableStateOf("")
+    }
+
+    val valueVc = remember {
+        mutableStateOf("")
+    }
+
+    val valuePass = remember {
+        mutableStateOf("")
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource("cadback.png"),
+                contentScale = ContentScale.FillBounds,
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Column (
+            Modifier
+                .padding(all = 24.dp)
+                .background(Color(0xFF202020))
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+
+            CustomTextInput(InputType.Gmail, value = valueGmail)
+            CustomTextInput(InputType.VC, value = valueVc)
+            CustomTextInput(InputType.Password, value = valuePass)
+            val buttonColor = remember {
+                mutableStateOf(Color(0xFFE5684A))
+            }
+            Button(
+                onClick = {buttonColor.value = Color(0xFFE5684A) },
+                colors = ButtonDefaults.buttonColors(
+                    Color(0xFFE5684A)
+                ),
+                modifier = Modifier
+                    .padding(bottom = 12.dp)
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+
+                ) {
+                    Text(
+                        text = "Send Gmail",
+                        color = Color.White
+                    )
+                }
+            }
+        }
+    }
+}
