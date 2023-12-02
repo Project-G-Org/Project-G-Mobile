c22ed4d665f2dc2bc7ff755f471243e00b7e6a0d
victorluiz3315 <140361500+victorluiz3315@users.noreply.github.com>
2023-12-02 15:43:52 -0300

    kkkkkkkkkkkkkkkk

--- Project-G-android/src/main/java/com/example/project_g_kmp/android/view/profile.kt
+++ Project-G-android/src/main/java/com/example/project_g_kmp/android/view/profile.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -206,7 +207,6 @@ fun Profile (modifier: Modifier = Modifier.background(Color(0xFFEBEBEB))){
                                    androidx.compose.material3.TextField(
                                        value = value,
                                        onValueChange = { value = it },
-                                       singleLine = true,
                                    )
 
                                 }
