451f9ccfc449ce15089fa33fd65b58bf6804b88a
victorluiz3315 <140361500+victorluiz3315@users.noreply.github.com>
2023-12-09 17:09:22 -0300

    Update like.kt

--- Project-G-android/src/main/java/com/example/project_g_kmp/android/view/like.kt
+++ Project-G-android/src/main/java/com/example/project_g_kmp/android/view/like.kt
@@ -1,4 +1,9 @@
 package com.example.project_g_kmp.android.view
 
-class like {
+import androidx.compose.runtime.MutableState
+import androidx.lifecycle.ViewModel
+
+class like : ViewModel() {
+
+    val _num = MutableStateOf(0)
 }
