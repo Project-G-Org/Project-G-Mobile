c2e5f56e4b7336c79fe9f2f9b5a34f2c1bd486dc
victorluiz3315 <140361500+victorluiz3315@users.noreply.github.com>
2023-12-20 15:29:28 -0300

    postSreen

--- composeApp/src/commonMain/kotlin/presentation/screens/PostProjectScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/PostProjectScreen.kt
@@ -1,2 +1,146 @@
 package presentation.screens
 
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.IconButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalResourceApi::class)
+@Composable
+fun PostProjectScreen (){
+
+    Box(
+        Modifier
+            .background(Color.White)
+            .fillMaxSize()){
+
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color(0xFF202020),
+            ),
+            modifier = Modifier
+                .padding(10.dp, 5.dp)
+                .fillMaxWidth(),) {
+            Column (
+                Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Top) {
+
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+
+                    IconButton(onClick = {  }) {
+
+                        Icon(
+                            painter = painterResource("baseline_close_24.xml"),
+                            contentDescription = null,
+                            Modifier.size(30.dp),
+                            tint = Color(0xFFE5684A)
+                        )
+                    }
+
+                    TextButton(onClick = {  }) {
+                        Text(
+                            "Post",
+                            Modifier.padding(5.dp),
+                            color = Color(0xFFE5684A),
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                    }
+                }
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    horizontalAlignment = Alignment.Start
+                ) {
+
+                    Row {
+
+                        Image(
+                            painter = painterResource("backlogin.png"),
+                            contentDescription = null,
+                            Modifier
+                                .size(55.dp)
+                                .clip(RoundedCornerShape(180.dp)),
+                            contentScale = ContentScale.FillBounds,
+
+                            )
+
+                        var post by remember { mutableStateOf("") }
+                        TextField(
+                            value = post,
+                            onValueChange = { post = it },
+                            Modifier.fillMaxWidth(),
+                            colors = TextFieldDefaults.textFieldColors(
+                                unfocusedIndicatorColor = Color(0xFF202020),
+                                unfocusedSuffixColor = Color(0xFF202020),
+                                focusedIndicatorColor = Color(0xFF202020),
+                                focusedSuffixColor = Color(0xFF202020),
+                                containerColor = Color(0xFF202020),
+                                unfocusedTextColor = Color.White,
+                                focusedTextColor = Color.White
+                            )
+                        )
+                    }
+                    Divider(
+                        modifier = Modifier
+                            .height(1.dp)
+                            .fillMaxWidth(), color = Color.Black
+                    )
+
+                    IconButton(onClick = { /*TODO*/ }) {
+
+                        Icon(
+                          painter = painterResource("baseline_add_photo_alternate_24.xml"),
+                            contentDescription = null,
+                            Modifier.size(30.dp),
+                            tint = Color(0xFFE5684A)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/presentation/screens/SearchScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/SearchScreen.kt
@@ -1,2 +1,302 @@
 package presentation.screens
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.IconButton
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalResourceApi::class)
+@Composable
+fun SearchScreen () {
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFEBEBEB))
+    ) {
+
+        Column(Modifier
+            .verticalScroll(rememberScrollState())) {
+
+            Box (
+                Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+            )   {
+
+                var pesquisa by remember { mutableStateOf("") }
+                androidx.compose.material3.TextField(
+                    value = pesquisa,
+                    onValueChange = { pesquisa = it },
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    colors = TextFieldDefaults.textFieldColors(
+                        unfocusedIndicatorColor = Color.White,
+                        unfocusedSuffixColor = Color.White,
+                        focusedIndicatorColor = Color.White,
+                        focusedSuffixColor = Color.White,
+                        containerColor = Color.White
+
+                    )
+                )
+
+                Row ( Modifier
+                    .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End){
+
+
+
+
+                    IconButton(onClick = { /*TODO*/ }, Modifier
+                        .size(56.dp)) {
+                        Icon(
+                           painter =  painterResource("baseline_search_24.xml"),
+                            contentDescription = null,
+                            Modifier.size(40.dp),
+                            tint = Color(0xFF202020)
+                        )
+                    }
+                }
+            }
+
+            Card(
+                modifier = Modifier
+                    .padding(10.dp, 5.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(18.dp)),
+                elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFF202020)
+                )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(all = 10.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+
+                    Row {
+
+                        Image(
+                            painter = painterResource("carr1.png"),
+                            contentDescription = null,
+                            Modifier
+                                .size(60.dp)
+                        )
+
+                        Column(
+                            modifier = Modifier
+                                .padding(3.dp, 10.dp)
+                        ) {
+
+                            Text(
+                                "marcelo",
+                                color = Color(0xFFE5684A),
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+
+                            Text(
+                                text = "MARANHAO",
+                                color = Color.White,
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+                        }
+                    }
+                    val buttonColor = remember {
+                        mutableStateOf(Color(0xFFE5684A))
+                    }
+                    Button(
+                        onClick = { buttonColor.value = Color(0xFFE5684A) },
+                        Modifier.padding(5.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            Color(0xFFE5684A)
+                        )
+                    )
+                    {
+                        Text(
+                            "PERFIL",
+                            color = Color(0xFF202020)
+                        )
+                    }
+                }
+            }
+
+            Card(
+                modifier = Modifier
+                    .padding(10.dp,5.dp,10.dp,70.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(18.dp)),
+                elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFF202020)
+                )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(all = 10.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+
+                    Row {
+
+                        Image(
+                            painter = painterResource("carr3.png"),
+                            contentDescription = null,
+                            Modifier
+                                .size(60.dp)
+                        )
+
+                        Column(
+                            modifier = Modifier
+                                .padding(3.dp, 10.dp)
+                        ) {
+
+                            Text(
+                                "marcelo",
+                                color = Color(0xFFE5684A),
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+
+                            Text(
+                                text = "MARANHAO",
+                                color = Color.White,
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+                        }
+                    }
+                    val buttonColor = remember {
+                        mutableStateOf(Color(0xFFE5684A))
+                    }
+                    Button(
+                        onClick = { buttonColor.value = Color(0xFFE5684A) },
+                        Modifier.padding(5.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            Color(0xFFE5684A)
+                        )
+                    )
+                    {
+                        Text(
+                            "PERFIL",
+                            color = Color(0xFF202020)
+                        )
+                    }
+                }
+            }
+        }
+    }
+    Column(
+        Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+
+        Row(
+            modifier = Modifier
+                .height(60.dp)
+                .fillMaxWidth()
+                .background(color = Color(0xFF202020)),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+
+            IconButton(onClick = {  }) {
+                Icon(
+                    painter = painterResource("baseline_home_24.xml"),
+                    contentDescription = null,
+                    Modifier.size(30.dp),
+                    tint = Color(0xFFE5684A)
+                )
+            }
+
+            IconButton(onClick = { }) {
+                Icon(
+                    painter = painterResource("baseline_apartment_24.xml"),
+                    contentDescription = null,
+                    Modifier.size(30.dp),
+                    tint = Color(0xFFE5684A)
+                )
+            }
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            {
+
+                IconButton(onClick = { }) {
+                    Icon(
+                        painter = painterResource("baseline_search_24.xml"),
+                        contentDescription = null,
+                        Modifier.size(30.dp),
+                        tint = Color(0xFFE5684A)
+                    )
+                }
+
+                Divider(
+                    modifier = Modifier
+                        .height(2.dp)
+                        .width(40.dp),
+                    color = Color(0xFFE5684A)
+                )
+
+            }
+
+
+            IconButton(onClick = { }) {
+                Icon(
+                    painter = painterResource("baseline_person_24.xml"),
+                    contentDescription = null,
+                    Modifier.size(30.dp),
+                    tint = Color(0xFFE5684A)
+                )
+            }
+        }
+    }
+}
